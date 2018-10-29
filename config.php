85cd65533094b19d9877f5a08d972fc626dbfb27
ghost9 <smuzairs@hotmail.com>
2018-10-29 11:30:45 +0500

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Sayyed Uzair',
         'nav_menu' => [
             '' => 'Home',
             'about-us' => 'About Us',
